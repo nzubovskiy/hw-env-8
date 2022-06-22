9c3eb92b3dc1281eaa0ba44a9e3f4325fba6e4a1
NIKO <nzubovskiy@gmail.com>
2022-06-22 17:08:55 +0400

    First commit

---
+++ src/main/java/com/example/hwenv8/HwEnv8Application.java
@@ -0,0 +1,13 @@
+package com.example.hwenv8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwEnv8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwEnv8Application.class, args);
+    }
+
+}
